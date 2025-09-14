7dbbf93bd4f9cbaa4c0a85dfa5eb493b9f256aeb
Josue Zea <jdzeaherrera@gmail.com>
2025-09-14 15:50:25 -0600

    adding files for publishing in pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export", // <--- importante para generar /out
+  basePath: "/portfolio", // <--- debe ser el nombre EXACTO de tu repo
+  assetPrefix: "/portfolio/", // <--- ayuda a que sirva bien en GitHub Pages
+  images: {
+    unoptimized: true, // <--- necesario porque no hay servidor en Pages
+  },
 }
 
 module.exports = nextConfig
