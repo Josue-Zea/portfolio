377ec7d687e4154c3af4c580f371ffaf9b705109
Josue Zea <jdzeaherrera@gmail.com>
2025-09-20 12:32:13 -0600

    Index updated for default lenguage

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale ?? "es", ['common'])),
     },
   }
 }
